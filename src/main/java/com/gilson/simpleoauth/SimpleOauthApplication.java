4a463992ddc71382b63c932ce628522e02f5222f
Gilson Modesto Da Silva Ferreira <modestogilson@oi.net.br>
2018-09-20 18:26:20 -0300

    adiconar botão logout

--- src/main/java/com/gilson/simpleoauth/SimpleOauthApplication.java
+++ src/main/java/com/gilson/simpleoauth/SimpleOauthApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,13 +26,20 @@ public class SimpleOauthApplication extends WebSecurityConfigurerAdapter{
 	}
 	
 	@Override
-	  protected void configure(HttpSecurity http) throws Exception {
-	    http
-	      .antMatcher("/**")
-	      .authorizeRequests()
-	        .antMatchers("/", "/login**", "/webjars/**", "/error**")
-	        .permitAll()
-	      .anyRequest()
-	        .authenticated();
-	  }
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.antMatcher("/**")
+		.authorizeRequests()
+		.antMatchers("/", "/login**", "/webjars/**", "/error**")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.logout()
+		.logoutSuccessUrl("/")
+		.permitAll()
+		.and()
+		.csrf()
+		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
 }
